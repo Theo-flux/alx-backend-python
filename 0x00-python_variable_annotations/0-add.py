3c1a70c2c5aaa09a96acf07238890dfa17d3d872
Theo <ekunnusitheophilus@gmail.com>
2023-07-06 14:23:42 +0100

    fix: Basic annotations - add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -3,5 +3,15 @@
 O-add.py
 """
 
+
 def add(a: float, b: float) -> float:
+    """adds two float numbers together
+
+    Args:
+        a (float): first float number
+        b (float): second float number
+
+    Returns:
+        float: returns addition of a and b
+    """
     return a + b
