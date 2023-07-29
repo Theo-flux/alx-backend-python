97dfadcb744254a140bf16bc4211f6183bacf655
Theo <ekunnusitheophilus@gmail.com>
2023-07-29 09:27:13 +0100

    fix: 0 Parameterize unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,33 +1,38 @@
 #!/usr/bin/env python3
-"""
-python test file for utils module
+"""A module for testing the utils module.
 """
 import unittest
-from unittest.mock import patch, MagicMock
+from typing import Dict, Tuple, Union, TypedDict
+from unittest.mock import patch, Mock
 from parameterized import parameterized
-from typing import Tuple, Union, TypedDict, Dict
 
-from utils import requests, access_nested_map, get_json
+from utils import (
+    access_nested_map,
+    get_json,
+    memoize,
+)
 
 
 TypedTestPayload = TypedDict('TypedTestPayload', {'payload': bool})
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Tests the `access_nested_map` function."""
+    """
+    test suite for utils.access_nested_map function
+    """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(
-            self,
-            nested_map: Dict,
-            path: Tuple[str],
-            expected: Union[Dict, int],
-            ) -> None:
-        """Tests `access_nested_map`'s output."""
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+        self,
+        nested_map: Dict,
+        path: Tuple[str],
+        output: Union[int, Dict]
+    ) -> None:
+        """utils.access_nested_map function"""
+        self.assertEqual(access_nested_map(nested_map, path), output)
 
     # @parameterized.expand([
     #     ({}, ("a",), KeyError('a')),
