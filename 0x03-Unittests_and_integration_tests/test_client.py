15378527427b09bc8355471f3793506ee26a15a5
Theo <ekunnusitheophilus@gmail.com>
2023-07-31 07:51:48 +0100

    fix: 4. Parameterize and patch as decorator

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -8,13 +8,14 @@ from client import GithubOrgClient
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    """Test suite for githuborgclient class"""
+    """Test suite for GithubOrgClient class"""
     @parameterized.expand([
         ('google'),
         ('abc')
     ])
     @patch('client.GithubOrgClient')
     def test_org(self, org_name, mocked_github_org_client):
+        """test case for GithubOrgClient.org method"""
         mocked_github_org_client(org_name)
         mocked_github_org_client.return_value.org.return_value = False
         mocked_github_org_client.assert_called_once_with(org_name)
