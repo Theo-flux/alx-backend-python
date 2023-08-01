16e8c76035c3641ddb421e2ce91f7269f4be0cd6
Theo <ekunnusitheophilus@gmail.com>
2023-08-01 02:52:00 +0100

    patch and parameterized decorators

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,6 +3,7 @@
 import unittest
 from unittest.mock import patch, MagicMock
 from parameterized import parameterized
+from typing import (Dict)
 
 from client import GithubOrgClient
 
@@ -10,11 +11,28 @@ from client import GithubOrgClient
 class TestGithubOrgClient(unittest.TestCase):
     """Test suite for githuborgclient class"""
     @parameterized.expand([
-        ('google'),
-        ('abc')
+        ('google', {'login': 'google'}),
+        ('abc', {'login', 'abc'})
     ])
-    @patch('client.GithubOrgClient')
-    def test_org(self, org_name, mocked_github_org_client):
-        mocked_github_org_client(org_name)
-        mocked_github_org_client.return_value.org.return_value = False
-        mocked_github_org_client.assert_called_once_with(org_name)
+    @patch('client.get_json')
+    def test_org(
+        self,
+        org_name: str,
+        out: Dict,
+        mocked_get_json: MagicMock
+    ) -> None:
+        """
+        test case for client.githubClientOrg.org() method
+
+        Args:
+            org_name (str): _description_
+            out (Dict): _description_
+            mocked_get_json (MagicMock): _description_
+        """
+        mocked_get_json.return_value = MagicMock()
+        mocked_get_json.return_value.return_value = out
+        test_github_org_client = GithubOrgClient(org_name)
+        self.assertEqual(test_github_org_client.org(), out)
+        mocked_get_json.assert_called_once_with(
+            f'https://api.github.com/orgs/{org_name}'
+        )
